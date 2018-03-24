bb18900bad3a95358dc197ef9eded907cfd74efb
Victor Pavel <victor.pavel25@gmail.com>
2018-03-24 23:17:52 +0000

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            //In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + a;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(Fibonacci(length));
+            Console.ReadKey();
+        }
+    }
+}
